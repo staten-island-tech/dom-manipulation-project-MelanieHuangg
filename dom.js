425165f0b3414a26611794185b01b7160faa4593
MelanieHuangg <melanie.huang0306@gmail.com>
2023-10-21 22:00:15 -0400

    index.html

--- dom.js
+++ dom.js
@@ -1,19 +1,36 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
-    foodName: document.querySelector(".food-name"),
+    gameName: document.querySelector(".game-name"),
+    h2: document.querySelector("h2")
    
 };
 
-console.log(DOMSelectors.h2s);
+console.log(DOMSelectors.h2);
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.foodName.value);
-    DOMSelectors.h2s.forEach(
-        (el)=> (el.textContent = DOMSelectors.foodName.value)
-    );
-
-
-
-
-
+    let userInput = DOMSelectors.gameName.value; //stores user input
+    const textBox = DOMSelectors.h2;
+    const game = makeGame(userInput); //has value gameObj
+    console.log(DOMSelectors.gameName.value); 
+    textBox.textContent = game.name
+   
+    // let h2 = document.createElement("h2");
+    // h2.textContent = game.name;
+    // document.body.appendChild(h2)
 });
+
+function makeGame (input){ //stores value of user's input
+    let gameObj = {
+        name: input
+    }
+
+    clearField()
+    return gameObj //allows you to use the function
+}
+
+//clearing input
+function clearField (){
+    DOMSelectors.gameName.value = ""
+}
+
+
